dff4aec9cfb9d8dc65643e6079832ab03976742b
Wes Nimmo <wesnimmo@gmail.com>
2026-05-12 12:53:57 -0700

    Configure Vercel serverless JSON server routing

--- src/contexts/QuizContext.js
+++ src/contexts/QuizContext.js
@@ -82,7 +82,7 @@ function QuizProvider({ children }) {
   );
 
   useEffect(function () {
-    fetch("http://localhost:9000/questions")
+    fetch("/api/questions")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataReceived", payload: data }))
       .catch((err) => dispatch({ type: "dataFailed" }));
